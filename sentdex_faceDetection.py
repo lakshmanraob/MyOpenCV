170b63c0fefd76f6edd2da52eb07f35e0dcbf728
Battula, Lakshmana Rao (US - Bengaluru) <lakshmanraob@gmail.com>
2017-06-02 10:13:52 +0530

    modified myexp

--- sentdex_faceDetection.py
+++ sentdex_faceDetection.py
@@ -1,29 +1,22 @@
+import cv2
+import numpy as np
+import picamera
 
 import io
-import picamera
-import cv2
-import numpy
 
-#Create a memory stream so photos doesn't need to be saved in a file
 stream = io.BytesIO()
 
-#Get the picture (low resolution, so it should be quite fast)
-#Here you can also specify other parameters (e.g.:rotate the image)
 with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.capture(stream, format='jpeg')
+    camera.resolution = (640,480)
+    camera.capture(stream,format='jpeg')
 
-#Convert the picture into a numpy array
-buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+buff = np.fromstring(stream.getvalue(),dtype=np.uint8)
 
-#Now creates an OpenCV image
-image = cv2.imdecode(buff, 1)
+image = cv2.imdecode(buff,1)
 
-#Load a cascade file for detecting faces
 face_cascade = cv2.CascadeClassifier('faces.xml')
 print face_cascade
 
-#Convert to grayscale
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 #Look for faces in the image using the loaded cascade file
@@ -37,3 +30,4 @@ for (x,y,w,h) in faces:
 
 #Save the result image
 cv2.imwrite('result.jpg',image)
+
